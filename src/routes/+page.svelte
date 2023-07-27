f7154b5099272642b68adf59b74dd069ab09f19a
Nicolas Morris (1000119863) <MORRNC1@student.op.ac.nz>
2023-07-27 13:01:31 +1200

    making a base website

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!DOCTYPE html>
+<html lang="en"> <!-- tells the browser what languge it is good for telling screen readters things -->
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- view poiint rescaling thats how i understand it anyways -->
+  <title>Nick's Website</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      text-align: center;
+    }
+    a {
+      color: #007BFF;
+      text-decoration: none;
+    }
+    a:hover {
+      color: #0056b3;
+      text-decoration: underline;
+    }
+  </style>
+</head>
+<body>
+  <h1>My name is Nick</h1>
+  <p>I like audiobooks and have 1 cat, 2 dogs, and 2 horses.</p>
+  <p>Check out <a href="https://www.audible.com/" target="_blank">Audible</a> for audiobooks!</p>
+</body>
+</html>
