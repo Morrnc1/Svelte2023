f3ce8da62051b7db76f86b0fec2a82019089b4d0
morrnc1 <morrnc1@student.op.ac.nz>
2023-08-01 10:52:37 +1200

    Messed up did not know what conventions to follow will start again

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Header from '$lib/Header.svelte';
+</script>
+
+<Header/>
+
+<footer>
+  <p>Audiobooks found here</p>
+  <p><a href="https://www.audible.com">Audible</a></p>
+</footer>
