40e85f0208bf39d6378d8fc63837b7ccd09be999
morrnc1 <morrnc1@student.op.ac.nz>
2023-08-01 09:47:05 +1200

    Got a few pages forgot to do reguler commits oof

---
+++ src/routes/Audiobook.svelte
@@ -0,0 +1,65 @@
+<!-- Audiobook Page -->
+<script>
+  export let title;
+  export let author;
+  export let description;
+  export let imagePath;
+</script>
+
+<style>
+  .book {
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+    padding: 10px;
+    background-color: #fff;
+    border-radius: 5px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  }
+
+  .book img {
+    max-width: 150px;
+    margin-right: 20px;
+    border-radius: 5px;
+  }
+
+  .book-info {
+    flex: 1;
+  }
+
+  .book-title {
+    font-size: 1.2rem;
+    font-weight: bold;
+    margin: 0;
+  }
+
+  .book-author {
+    font-size: 1rem;
+    color: #888;
+  }
+
+  .book-description {
+    margin-top: 10px;
+    color: #555;
+  }
+
+  a {
+    display: inline-block;
+    margin-top: 10px;
+    background-color: #007BFF;
+    color: #fff;
+    padding: 8px 16px;
+    text-decoration: none;
+    border-radius: 5px;
+  }
+</style>
+
+<div class="book">
+  <img src={imagePath} alt={title}>
+  <div class="book-info">
+    <h2 class="book-title">{title}</h2>
+    <p class="book-author">Author: {author}</p>
+    <p class="book-description">{description}</p>
+    <a href="#">More Info</a>
+  </div>
+</div>
---
+++ src/routes/AudiobookDetail.svelte
@@ -0,0 +1,57 @@
+<!-- AudiobookDetail Page -->
+<script>
+  export let audiobook;
+</script>
+
+<style>
+  header {
+    background-color: #333;
+    color: #fff;
+    padding: 20px;
+    text-align: center;
+  }
+
+  h1 {
+    margin: 0;
+  }
+
+  main {
+    padding: 20px;
+  }
+
+  h2 {
+    font-size: 1.4rem;
+    margin-bottom: 10px;
+  }
+
+  p {
+    margin: 0;
+    margin-bottom: 10px;
+  }
+
+  .go-back {
+    display: inline-block;
+    margin-top: 10px;
+    background-color: #007BFF;
+    color: #fff;
+    padding: 8px 16px;
+    text-decoration: none;
+    border-radius: 5px;
+  }
+</style>
+
+<header>
+  <h1>{audiobook.title}</h1>
+</header>
+
+<main>
+  <h2>About the Audiobook</h2>
+  <p>Author: {audiobook.author}</p>
+  <p>Description: {audiobook.description}</p>
+  <p>Publishing Date: {audiobook.publishingDate}</p>
+  <p>Narrator: {audiobook.narrator}</p>
+
+  <!-- more detailed here please me  -->
+
+  <a href="javascript:history.back()" class="go-back">Go Back</a>
+</main>
