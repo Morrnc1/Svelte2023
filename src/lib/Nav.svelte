4ac5a737be5ff22d91e2ffd5a8bd549c5a315ed0
morrnc1 <morrnc1@student.op.ac.nz>
2023-09-29 11:29:02 +1300

    Adding to website design

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,39 +1,45 @@
+<!-- navbar.svelte -->
 <head>
   <link rel="stylesheet" href="css/nav.css">
 </head>
 
 <script>
+  import { searchTerm } from "$lib/store.js";  // Include searchTerm
   let showDropdown = false;
 
   function toggleDropdown() {
       showDropdown = !showDropdown;
   }
 
- 
   function closeDropdown() {
       showDropdown = false;
+  }
+
+  function updateSearch(e) {
+      searchTerm.set(e.target.value);  // Update the searchTerm store
   }
 </script>
 
 <nav class="navbar">
   <div class="nav-logo">
-      <a href="/" on:click={closeDropdown}>
-          <img class="logo" src="https://toppng.com/uploads/preview/a-little-maid-neko-for-today-kawaii-neko-girl-chibi-11562928180umulz4qslj.png" alt="Site Logo">
-      </a>
-    </div>
-    
-    <div class="container">
-      <input type="text" placeholder="Search...">
-      <div class="search"></div>
-    </div>
+    <a href="/" on:click={closeDropdown}>
+        <img class="logo" src="https://toppng.com/uploads/preview/a-little-maid-neko-for-today-kawaii-neko-girl-chibi-11562928180umulz4qslj.png" alt="Site Logo">
+    </a>
+  </div>
+  
+  <div class="container">
+    <input type="text" placeholder="Search..." on:input={updateSearch}> <!-- Add on:input handler -->
+    <div class="search"></div>
+  </div>
 
   <div class="menu-icon" on:click={toggleDropdown}>
-      ☰
+    ☰
   </div>
+  
   <div class={showDropdown ? 'dropdown-content show' : 'dropdown-content'}>
-      <a href="/home" on:click={closeDropdown}>Home</a>
-      <a href="/about" on:click={closeDropdown}>About</a>
-      <a href="/books" on:click={closeDropdown}>Books</a>
+    <a href="/home" on:click={closeDropdown}>Home</a>
+    <a href="/about" on:click={closeDropdown}>About</a>
+    <a href="/books" on:click={closeDropdown}>Books</a>
   </div>
 
 </nav>
--- src/routes/books/+page.svelte
+++ src/routes/books/+page.svelte
@@ -1,26 +1,16 @@
-<head>
-  <link rel="stylesheet" href="css/books.css">
-</head>
+<!-- books.svelte -->
 <script>
-  let books = [
-    { title: 'The Primal Hunter', author: 'Zogarth', image: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1642013459i/60111529.jpg' },
-    { title: 'Defiance Of The Fall', author: 'TheFirstDefier', image: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1619486020i/57866482.jpg' },
-    { title: 'He Who Fights With Monsters', author: 'Shirtaloon', image: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1614114118i/57189884.jpg' },
-    { title: 'All The SKills', author: 'Honour Rae', image: 'https://www.royalroadcdn.com/public/covers-large/55687-all-the-skills-a-deckbuilding-litrpg.jpg?time=1660799721' },
-    { title: 'Dungeon Carawler Carl', author: 'Matt Dinniman', image: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1595600712i/54659324.jpg' },
-    { title: 'System Universe', author: 'SunriseCV', image: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1658172758i/61588227.jpg' },
-    
-  ];
+  import { books, selectedGenre, searchTerm } from "$lib/store.js";  // Include searchTerm
 </script>
-
+<link rel="stylesheet" href="css/books.css" />
 <main class="book-container">
-  {#each books as book}
-    <div class="book-card">
-      <img src={book.image} alt={book.title} />
-      <h3>{book.title}</h3>
-      <p>{book.author}</p>
-    </div>
+  {#each $books as book}
+    {#if ($selectedGenre === "All" || book.genre === $selectedGenre) && (book.title.toLowerCase().includes($searchTerm.toLowerCase()) || book.author.toLowerCase().includes($searchTerm.toLowerCase()) || book.genre.toLowerCase().includes($searchTerm.toLowerCase()))}
+      <div class="book-card">
+        <img src={book.image} alt={book.title} />
+        <h3>{book.title}</h3>
+        <p>{book.author}</p>
+      </div>
+    {/if}
   {/each}
 </main>
-
-
