8c77ac84622bb7e468b581f4e4d37046b58d5027
morrnc1 <morrnc1@student.op.ac.nz>
2023-10-19 22:41:56 +1300

    making new nav

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -21,25 +21,26 @@
 </script>
 
 <nav class="navbar">
-  <!-- <div class="nav-logo">
-    <a href="/" on:click={closeDropdown}>
-        <img class="logo" src="https://toppng.com/uploads/preview/a-little-maid-neko-for-today-kawaii-neko-girl-chibi-11562928180umulz4qslj.png" alt="Site Logo">
-    </a>
-  </div>
+  <!-- 
+</div>
+
+<div class="container">
+  <input type="text" placeholder="Search..." on:input={updateSearch}> 
+  <div class="search"></div>
+</div> -->
+
+  <div class="nav-logo">
+  <a href="/" on:click={closeDropdown}>
+      <img class="logo" src="https://toppng.com/uploads/preview/a-little-maid-neko-for-today-kawaii-neko-girl-chibi-11562928180umulz4qslj.png" alt="Site Logo">
+  </a>
   
-  <div class="container">
-    <input type="text" placeholder="Search..." on:input={updateSearch}> 
-    <div class="search"></div>
-  </div> -->
-  
+
    <ul>
-    <li><aclass="" href="">Welcome</a></li>
+    <li><a class="" href="">Welcome</a></li>
     <li><a href="">About</a></li>
     <li><a href="">Books</a></li>
     <li><a href="">Home</a></li>
    </ul>
-
-  
 
 
 </nav>
