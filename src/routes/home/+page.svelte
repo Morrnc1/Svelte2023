fb9dc8f7d2507d6f0a7d19f8c20d9c297480d3f2
morrnc1 <morrnc1@student.op.ac.nz>
2023-10-23 13:53:55 +1300

    adding to home  page

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,26 +1,15 @@
 <script>
-    import { selectedGenre } from "$lib/store.js";
-    let selected;
-    $selectedGenre = "All"; 
-  
-    function changeGenre(genre) {
-      selectedGenre.set(genre);
-    }
-  </script>
-  
-  <div class="container">
-    <div class="heading">
-      <h2>Welcome to the Library!</h2>
-    </div>
-  
-    <div class="genre-section">
-      <button on:click={() => changeGenre('All')} class="genre-btn">All</button>
-      <button on:click={() => changeGenre('Fantasy')} class="genre-btn">Fantasy</button>
-      <button on:click={() => changeGenre('Science Fiction')} class="genre-btn">Science Fiction</button>
-    </div>
-  
-    <div class="book-of-the-month">
-      <h3>Book of the Month</h3>
-    </div>
-  </div>
-  
+  import { books } from '$lib/store.js';
+  let bookList = [];
+
+  books.subscribe(value => {
+    bookList = value;
+  });
+</script>
+
+<style>
+</style>
+
+<div id="banner">
+  <h1>Explore a Universe of Books</h1>
+</div>
