d5c1fc0b32f3100836b5e2d1460f4b5e6bc0fbeb
morrnc1 <morrnc1@student.op.ac.nz>
2023-10-23 13:58:00 +1300

    adding to home  page

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { books } from '$lib/store.js';
+  import { books } from './bookStore.js';
   let bookList = [];
 
   books.subscribe(value => {
@@ -7,9 +7,22 @@
   });
 </script>
 
-<style>
-</style>
-
 <div id="banner">
   <h1>Explore a Universe of Books</h1>
+</div>
+
+<div id="content">
+  <div id="top-three">
+    <h2>Top Three</h2>
+    {#each bookList.slice(0, 3) as book}
+      <div class="book">
+        <img src={book.image} alt={book.title} />
+        <div>
+          <h3>{book.title}</h3>
+          <p>Author: {book.author}</p>
+          <p>Genre: {book.genre}</p>
+        </div>
+      </div>
+    {/each}
+  </div>
 </div>
