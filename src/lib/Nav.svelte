7cce66a7deb0194b751ee814dc494e5144559061
morrnc1 <morrnc1@student.op.ac.nz>
2023-10-23 18:12:05 +1300

    adding the start of a toggle side bar

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,5 +1,18 @@
 <div class="toggle" on:click={toggleMenu}></div>
 <div class={`sidebar ${showMenu ? 'show' : ''}`} on:mouseleave={collapseMenu}>
     <h3>Menu</h3>
-    
+    <ul>
+        <li><a href="/home" on:click={collapseMenu}>Home</a></li>
+        <li>
+            <a href="/books">Books</a>
+            <div class="submenu">
+                <ul>
+                    <li><a href="/books?genre=litrpg" on:click={collapseMenu}>LitRPG</a></li>
+                    <li><a href="/books?genre=apocalypse" on:click={collapseMenu}>Apocalypse</a></li>
+                    <li><a href="/books?genre=cultivation" on:click={collapseMenu}>Cultivation</a></li>
+                </ul>
+            </div>
+        </li>
+        <li><a href="/about" on:click={collapseMenu}>About</a></li>
+    </ul>
 </div>
