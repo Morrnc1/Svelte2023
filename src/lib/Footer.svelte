a8af26618c761d583c72ee4dfe4f6af5d9980265
morrnc1 <morrnc1@student.op.ac.nz>
2023-11-02 23:33:11 +1300

    working on footer styling

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -3,23 +3,26 @@
 
 <footer class="footer">
   <div class="codetheworld-io">
-    <a href="#" style="color: #0072b1">
-      <i class="fa-brands fa-linkedin-in"></i>
-    </a>
-  
-    <a href="#" style="color: #E1306C">
-      <i class="fa-brands fa-instagram"></i>
-    </a>
-  
-    <a href="#" style="color: #FF0050">
-      <i class="fa-brands fa-tiktok"></i>
-    </a>
-  
-    <a href="#" style="color: #4267B2">
-      <i class="fa-brands fa-facebook-f"></i>
-    </a>
+    <div class="codetheworld-io">
+      <a href="#" class="linkedin" style="--color: #0072b1;">
+        <i class="fa-brands fa-linkedin-in"></i>
+      </a>
+    
+      <a href="#" class="instagram" style="--color: #E1306C;">
+        <i class="fa-brands fa-instagram"></i>
+      </a>
+    
+      <a href="#" class="tiktok" style="--color: #FF0050;">
+        <i class="fa-brands fa-tiktok"></i>
+      </a>
+    
+      <a href="#" class="facebook" style="--color: #4267B2;">
+        <i class="fa-brands fa-facebook-f"></i>
+      </a>
   </div>
 </footer>
 
+
+  
 
   
