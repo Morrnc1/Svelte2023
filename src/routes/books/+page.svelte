2f7278bf2709c15cb82b00fe714c9c3d973f91ba
morrnc1 <morrnc1@student.op.ac.nz>
2023-11-09 18:04:34 +1300

    adding search bar ro booksa page

--- src/routes/books/+page.svelte
+++ src/routes/books/+page.svelte
@@ -1,17 +1,40 @@
 <script>
-  import { books } from "$lib/store.js";
-
+  import { books, searchTerm } from "$lib/store.js";
+  
   let bookArray = [];
+  let filterTerm = '';
 
+  // Subscription to the books store
   books.subscribe((value) => {
     bookArray = value;
   });
+
+  // Subscription to the search term store
+  searchTerm.subscribe((value) => {
+    filterTerm = value.toLowerCase();
+  });
+
+  // Derived array based on the search term
+  $: filteredBooks = bookArray.filter((book) =>
+    book.title.toLowerCase().includes(filterTerm) ||
+    book.author.toLowerCase().includes(filterTerm) ||
+    book.genre.toLowerCase().includes(filterTerm)
+  );
 </script>
 
 <link rel="stylesheet" href="css/books.css" />
 
+<div class="search-container">
+  <input
+    type="text"
+    class="search-bar"
+    placeholder="Search by name, author, or genre..."
+    on:input="{(e) => searchTerm.set(e.target.value)}"
+  />
+</div>
+
 <main class="book-container">
-  {#each bookArray as book}
+  {#each filteredBooks as book}
     <div class="book-card">
       <img src={book.image} alt={book.title} />
       <h3>{book.title}</h3>
